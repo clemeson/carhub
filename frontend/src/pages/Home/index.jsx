b0a0766c4ec07bb836f776f467c0e0b0a8a0748e
clemeson <clemeson4lves@gmail.com>
2024-11-07 10:51:31 -0300

    fix: responsive title h1

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -9,8 +9,8 @@ function Home() {
       <div className="bg-primary" >
         <section className="section1 d-flex flex-column vh-100 container pt-5  " data-aos="fade-up"   data-aos-duration="1500" >
           <div className="row">
-            <div className="col-md-6 d-flex flex-column mt-5 pt-5 ">
-              <h1 className="fs-1 fw-bold text-white mt-5">
+            <div className="col-md-6 d-flex flex-column  pt-5">
+              <h1 className=" fw-bold text-white mt-5">
                 Facilitamos sua vida
               </h1>
               <h3 className="fw-semibold">
@@ -23,11 +23,11 @@ function Home() {
               </h5>
             </div>
 
-            <div className="col-md-6 d-flex justify-content-center mt-5">
+            <div className="col-md-6 d-flex justify-content-center mt-5 order-md-1">
               <img className="w-100" src={car1} alt="Carro 1"  data-aos="flip-left"   />
             </div>
           </div>
-          <div className="mt-3 pt-3">
+          <div className="mt-3 pt-3 order-md-2" >
             {" "}
             <Forms />
 
@@ -38,7 +38,7 @@ function Home() {
         <section className="section2 vh-100 bg-white" >
           <div className="row container mx-auto pt-5" data-aos="fade-right" data-aos-duration="1500" >
             <div className="col-md-6 d-flex flex-column mt-5 pt-5">
-              <h1 className="fs-1 fw-bold mt-5">Ganhe Dinheiro Extra Com Seu Carro</h1>
+              <h1 className=" fw-bold mt-5">Ganhe Dinheiro Extra Com Seu Carro</h1>
               <h3 className="fw-semibold">
               Transforme seu carro em uma fonte de renda
               </h3>
