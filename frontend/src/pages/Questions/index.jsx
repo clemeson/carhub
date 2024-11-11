96751b82710fc50b1ce85b5c0c01f2a87d574d21
clemeson <clemeson4lves@gmail.com>
2024-11-11 13:31:37 -0300

    feat: page placeholder

--- frontend/src/pages/Questions/index.jsx
+++ frontend/src/pages/Questions/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-
+import constru from '../../assets/constructor.svg';
+import Button from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 // import { Container } from './styles';
 
 function Questions() {
-  return <h1>Perguntas Frequents</h1>;
+  const navigate = useNavigate();
+  const handleButton = (e) => {
+    e.preventDefault();
+
+    navigate('/');
+  };
+  return (
+    <>
+      <div className="container d-flex flex-column justify-content-center w-100">
+        <img
+          style={{ maxWidth: '600px' }}
+          className="w-100 mx-auto"
+          src={constru}
+        ></img>
+        <h1 className="fw-bold mx-auto">Esta pagina está em construção...</h1>
+        <div className="w-50 mx-auto">
+          <Button
+            color={'primary'}
+            text={'Voltar para Home'}
+            handleClick={handleButton}
+          ></Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Questions;
